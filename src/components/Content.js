22c2f36dbc60e6f18ec9b976fd0673e27425c0f0
mostafa1411 <mostafa.abdullah1411@gmail.com>
2022-08-30 12:35:55 +0200

    Clear debugging logs

--- src/components/Content.js
+++ src/components/Content.js
@@ -5,9 +5,6 @@ import ExploreButton from './ExploreButton';
 import Cards from './Cards';
 
 const Content = ({ title, description, courses }) => {
-    console.log(title);
-    console.log(description);
-    console.log(courses);
     return (
         <div className="content">
             <SectionTitle title={title} />
